182015dda716dee1e5427cbd9a00bebfe68a5050
Davi Lima <davidamartesgit@Gmail.com>
2024-08-16 18:37:59 -0300

    Add DI de TenisService

--- BackendApi/Program.cs
+++ BackendApi/Program.cs
@@ -1,4 +1,5 @@
 using BackendApi.Data;
+using BackendApi.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITenisService, TenisService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {

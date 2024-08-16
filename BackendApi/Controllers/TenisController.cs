8aa979a1b3af8e9c01de9a54037f6eac0096eb28
Davi Lima <davidamartesgit@Gmail.com>
2024-08-16 18:19:22 -0300

    Remove controller e adicionar um novo Controller para operações na Entidade Tenis

---
+++ BackendApi/Controllers/TenisController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackendApi.Data;
+using BackendApi.Models;
+
+namespace BackendApi.Controllers
+{
+    public class TenisController : ControllerBase
+    {
+        
+
+    }
+}

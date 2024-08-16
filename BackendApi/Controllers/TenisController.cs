94b977b9b303b4e9958a0b72e1b50800c171ac38
Davi Lima <davidamartesgit@Gmail.com>
2024-08-16 18:49:45 -0300

    Add Método do controller para buscar Tênis

--- BackendApi/Controllers/TenisController.cs
+++ BackendApi/Controllers/TenisController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BackendApi.Data;
-using BackendApi.Models;
+﻿using BackendApi.Service;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace BackendApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class TenisController : ControllerBase
     {
-        
+        private readonly ITenisService _tenisService;
+        public TenisController(ITenisService tenisService)
+        {
+            _tenisService = tenisService;
+        }
 
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTenis( )
+        {
+            var tenis = await _tenisService.ObterTenis();
+            if(tenis == null)
+            {
+                return NotFound();
+            }
+            return Ok(tenis);
+        }
     }
 }

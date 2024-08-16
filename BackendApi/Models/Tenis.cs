5321517524c79098abdd0001a07f49d185368831
Davi Lima <davidamartesgit@Gmail.com>
2024-08-16 17:39:29 -0300

    Add validações aos campos

--- BackendApi/Models/Tenis.cs
+++ BackendApi/Models/Tenis.cs
@@ -1,8 +1,15 @@
-﻿namespace BackendApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendApi.Models
 {
     public class Tenis : Entity
     {
+        [Required]
+        [StringLength(80, ErrorMessage = "Limite de 80 caracteres")]
         public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, 10000, ErrorMessage = "O preço deve ser um valor positivo.")]
         public decimal Preco { get; set; }
 
     }

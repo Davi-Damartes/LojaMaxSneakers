3214526905a87de003f2dd3ee05231ef5162b15f
Davi Lima <davidamartesgit@Gmail.com>
2024-08-16 16:36:18 -0300

    Add Models para reprensentar o objeto Tênis e Marca.

---
+++ BackendApi/Models/Tenis.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendApi.Models
+{
+    public class Tenis : Entity
+    {
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+
+    }
+}

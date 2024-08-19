e078ace05080beda23db10a6a7ff8f747753c2aa
Davi Lima <davidamartesgit@Gmail.com>
2024-08-19 19:33:37 -0300

    Remodelagem do Header

--- clientreact/src/Components/Header/index.js
+++ clientreact/src/Components/Header/index.js
@@ -9,25 +9,33 @@ function Header() {
     return (
         <header className={styles.header}>
             <img className={styles.logo} src={logo} />
+                 
+            <nav>
+                <ul className={styles.navlinks}>
+                    <li className={styles.li}>
+                        <a href="/">Nike</a>
+                    </li>
+                    <li className={styles.li}>
+                        <a href="/">Adidas</a>
+                    </li>
+                    <li className={styles.li}>
+                        <a href="/">Jordan</a>
+                    </li>
 
-            <div class={styles.line} />     
-                <Typography className={styles.titulos} gutterBottom>Nike</Typography>
-                <Typography className={styles.titulos} gutterBottom>Adidas</Typography>
-                <Typography className={styles.titulos} gutterBottom>Jordan</Typography>
-         
-            <div class={styles.line} />
-                <Button class={styles.botaoIcone} aria-label="search" color="primary">
-                    <SearchIcon class={styles.Icones}  />
+                </ul>
+            </nav>  
+
+            <div className={styles.iconButtons}>
+                <Button className={styles.botaoIcones} aria-label="search" >
+                    <SearchIcon />
                 </Button>
-            <div class={styles.line} />
-                <Button class={styles.botaoIcone} aria-label="search" color="primary">
-                    <FavoriteBorderIcon class={styles.Icones}  />
+                <Button className={styles.botaoIcones} aria-label="search" >
+                    <FavoriteBorderIcon />
                 </Button>
-            <div class={styles.line} />
-                <Button class={styles.botaoIcone} aria-label="search" color="primary">
-                    <ShoppingCartOutlinedIcon class={styles.Icones} />
+                <Button className={styles.botaoIcones} aria-label="search" >
+                    <ShoppingCartOutlinedIcon />
                 </Button>
-            <div class={styles.line} />
+            </div>
         </header>
 
     );

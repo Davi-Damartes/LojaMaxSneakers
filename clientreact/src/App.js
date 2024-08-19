481624e2fe173c1d2eb59cd1eb9edb72118f3c90
Davi Lima <davidamartesgit@Gmail.com>
2024-08-19 19:36:54 -0300

    Chama dos componentes para exibir montar o Layout

--- clientreact/src/App.js
+++ clientreact/src/App.js
@@ -1,14 +1,42 @@
-import { Typography } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
 import Header from './Components/Header';
+import Container from './Components/Container';
+import CarouselTenis from './Components/CarouselTenis';
+import CardTenis from './Components/CardTenis/index';
 
-  
+import tenisBranco from "./imagemTenis/TenisBranco.jpeg";
+import tenisLaranja from "./imagemTenis/TenisLaranja.jpeg";
+import tenisPretoRosa from "./imagemTenis/TenisPretoRosa.jpeg";
+import Footer from './Components/Footer';
+
+
 function App() {
   return (
     <div className="App">
       <Header />
-       
+      <CarouselTenis />
+      <Container >
+         { <CardTenis imagens={tenisPretoRosa} /> }
+
+         { <CardTenis imagens={tenisLaranja}/> }
+
+         { <CardTenis imagens={tenisBranco} /> }
+
+         { <CardTenis imagens={tenisLaranja} /> }
+
+         { <CardTenis imagens={tenisPretoRosa} /> }
+
+         { <CardTenis imagens={tenisBranco} /> }
+
+         { <CardTenis imagens={tenisLaranja} /> }
+
+         { <CardTenis imagens={tenisPretoRosa} /> }
+
+      </Container>
+
+      <Footer />
     </div>
   );
 }

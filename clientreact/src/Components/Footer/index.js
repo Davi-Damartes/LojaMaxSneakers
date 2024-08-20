44a906d301432ba9098594fb72135b7555260994
Davi Lima <davidamartesgit@Gmail.com>
2024-08-20 14:42:46 -0300

    Add descrição para o footer da página

--- clientreact/src/Components/Footer/index.js
+++ clientreact/src/Components/Footer/index.js
@@ -3,7 +3,9 @@ import styles from "./Footer.module.css"
 function Footer(){
 
     return (
-        <footer className={styles.footer}>Teste</footer>
+        <footer className={styles.footer}>
+              <h2>MaxSneakersDrop &copy; Desenvolvido por David Braga 2024</h2>
+        </footer>
     )
 }
 

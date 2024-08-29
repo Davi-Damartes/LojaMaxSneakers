10bd683b61bd117d472e6ec2a666e94f5ebb8e0e
Davi Lima <davidamartesgit@Gmail.com>
2024-08-29 19:33:59 -0300

    Add rotas

--- clientreact/src/index.js
+++ clientreact/src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from './pages/Home';
+import DetalhesTenis from './pages/DetalhesTenis';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div style={{display: 'flex', justifyContent: 'center', }}>
+                    <h1>Not Found!</h1>
+                  </div>
+  },
+  {
+    path: "/Detalhes",
+    element: <DetalhesTenis />
+  },
+  {
+    path: "/1",
+    element:<div>
+              <h2>Teste</h2>
+              <h2>Teste2</h2>
+            </div>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

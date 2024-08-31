a2ccca9f2b6f848fd4f1d8db07f2d52d884dcad5
Davi Lima <davidamartesgit@Gmail.com>
2024-08-31 18:31:48 -0300

    Add link para Redirecionar para detalhes do Tênis

--- clientreact/src/Components/CardTenis/index.js
+++ clientreact/src/Components/CardTenis/index.js
@@ -2,30 +2,33 @@ import styles from "./CardTenis.module.css"
 import Card from 'react-bootstrap/Card';
 import ModalEditarTenis from "../ModalEditarTenis";
 import ModalExcluirTenis from "../ModalExcluirTenis";
+import { Link } from "react-router-dom";
 
 function CardTenis( {imagens, id, nome, descricao, preco } ) {
     return(
         <section>
-            <Card className={styles.card} >
-                <Card.Img className={styles.imagem} src={imagens} />
-                <Card.Body className={styles.cardBody}>
-                    <Card.Title className={styles.title}>{nome}</Card.Title>
-                    <Card.Text className={styles.descricao}>{descricao}</Card.Text>
-                    <Card.Text className={styles.price}>R$ {preco}</Card.Text>
-                    <div className={styles.buttonContainer}>
-                        <ModalEditarTenis
-                            id={id} 
-                            nome={nome}
-                            descricao={descricao}
-                            preco={preco}
-                        />
-                        <ModalExcluirTenis 
-                            id={id}
-                            nome={nome}
-                        />
-                    </div>
-                </Card.Body>
-            </Card>
+            <a href="/Detalhes">
+                <Card className={styles.card} >
+                    <Card.Img className={styles.imagem} src={imagens} />
+                    <Card.Body className={styles.cardBody}>
+                        <Card.Title className={styles.title}>{nome}</Card.Title>
+                        <Card.Text className={styles.descricao}>{descricao}</Card.Text>
+                        <Card.Text className={styles.price}>R$ {preco}</Card.Text>
+                        <div className={styles.buttonContainer}>
+                            <ModalEditarTenis
+                                id={id} 
+                                nome={nome}
+                                descricao={descricao}
+                                preco={preco}
+                            />
+                            <ModalExcluirTenis 
+                                id={id}
+                                nome={nome}
+                            />
+                        </div>
+                    </Card.Body>
+                </Card>
+            </a>
         </section>
     )
 }
